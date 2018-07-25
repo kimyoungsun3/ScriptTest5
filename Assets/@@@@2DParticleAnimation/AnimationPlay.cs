df4945949c00d6504d53fe0a6d9225a839871196
kimyoungsun3 <kyssmart@naver.com>
2018-07-26 00:03:28 +0900

    particle animation
    particle animation

---
+++ Assets/@@@@2DParticleAnimation/AnimationPlay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPlay : MonoBehaviour {
+	
+
+	void Start(){
+		Debug.Log ("key 1 animation player");
+	}
+
+
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Alpha1)) {
+			Animation ani = GetComponent<UnityEngine.Animation> ();
+			ani.Play (ani.clip.name);
+			//yield return new WaitForSeconds (ani.clip.length);
+		}		
+	}
+}
