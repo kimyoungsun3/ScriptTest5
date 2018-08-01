541fdf5c69be0f104bab906471ae9aeb60f4e4cb
kimyoungsun3 <kyssmart@naver.com>
2018-08-01 22:24:04 +0900

    Painter
    Painter

---
+++ Assets/Painter/Painter.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Painter{
+	public class Painter : MonoBehaviour {
+		Texture2D texture;
+		public int width = 256;
+		public int height = 256;
+
+		int[] buffer;
+
+		// Use this for initialization
+		void Start () {
+			texture = new Texture2D (width, height);
+			GetComponent<Renderer> ().material.mainTexture = texture;
+			buffer = new int[width * height];
+		}
+
+		Ray ray;
+		RaycastHit hit;
+		//Vector3 hitPoint;
+		//Bounds bounds;
+		void Update(){
+			if (Input.GetMouseButton (0)) {
+				ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				if (Physics.Raycast (ray, out hit, 100f)) {
+					CalculateColor (hit.lightmapCoord);
+
+
+					//hitPoint = hit.point;
+					//bounds = hit.collider.bounds;
+					//Debug.Log (
+					///hit.barycentricCoordinate
+					//+ ":" + hit.lightmapCoord
+					//+ ":" + hit.textureCoord
+					//+ ":" + hit.textureCoord2
+					//);
+				}
+			} else if (Input.GetMouseButtonDown (1)) {
+				Clear ();
+			}
+		}
+
+		Color color = new Color(1f, 0f, 0f);
+		Color colorClear = new Color(1f, 1, 1f);
+		void CalculateColor(Vector2 _point){
+			int x = (int)(_point.x * width);
+			int y = (int)(_point.y * height);
+			//Debug.Log (x + ":" + y);
+			texture.SetPixel( x, y, color);
+			texture.Apply ();
+		}
+
+		void Clear(){
+			//texture.SetPixels(0, 0, width, height, new Color[]{new Color(1, 1,1)});
+			//texture.SetPixel(new Color[]{new Color(1f, 1f,1f)});	
+			for (int x = 0; x < width; x++) {
+				for (int y = 0; y < height; y++) {					
+					texture.SetPixel(x, y, colorClear);
+				}
+			}
+			texture.Apply ();
+		}
+	}
+}
---
+++ Assets/PerlinNoise/PerlinNoise.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PerlinNoise{
+	public class PerlinNoise : MonoBehaviour {
+		public int width = 256;
+		public int height = 256;
+		public float scale = 20f;
+		public float offsetX = 100f;
+		public float offsetY = 100f;
+		Material material;
+		Texture2D texture;
+
+		void Start () {
+			offsetX = Random.Range (0f, 99999f);
+			offsetY = Random.Range (0f, 99999f);
+
+			material = GetComponent<Renderer> ().material;
+			texture = new Texture2D (width, height);
+			material.mainTexture = texture;
+		}
+
+		void Update(){
+			GenerateTexture ();
+		}
+
+		Texture2D GenerateTexture(){
+			Color _color;
+			for (int x = 0; x < width; x++) {
+				for (int y = 0; y < height; y++) {
+					_color = CalculateColor (x, y);
+					texture.SetPixel(x, y, _color);
+				}
+			}
+
+			texture.Apply ();
+			return texture;
+		}
+
+		Color CalculateColor(int x, int y){
+			float _xCoord = (float)x / width * scale + offsetX;
+			float _yCoord = (float)y / height * scale + offsetY;
+
+			float _c = Mathf.PerlinNoise (_xCoord, _yCoord);
+			return new Color (_c, _c, _c);
+		}
+
+
+
+	}
+}
