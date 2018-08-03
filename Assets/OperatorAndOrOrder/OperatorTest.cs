245efa5c7af8d628199651e112eeb345a16df5ef
kimyoungsun <kyssmart@naver.com>
2018-08-03 09:34:00 +0900

    222
    22

---
+++ Assets/OperatorAndOrOrder/OperatorTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OperatorAndOrOrder{
+	public class OperatorTest : MonoBehaviour {
+		public bool rtn = true;
+		
+		void Start () {
+			Debug.Log (string.Format("=========={0}===========", rtn));
+
+			bool _b;
+			Debug.Log ("1. A && B || C && D");
+			_b = Fun ("A") && Fun ("B") || Fun ("C") && Fun ("D");
+
+			Debug.Log ("2. (A && B) || (C && D)");
+			_b = (Fun ("A") && Fun ("B")) || (Fun ("C") && Fun ("D"));
+
+
+			Debug.Log ("3. A && (B || C) && D");
+			_b = Fun ("A") && (Fun ("B") || Fun ("C")) && Fun ("D");
+		}
+
+		bool Fun(int _var){
+			Debug.Log (_var);
+			return rtn;
+		}
+
+		bool Fun(string _str){
+			Debug.Log (_str);
+			return rtn;
+		}
+	}
+}
