06dc5308c67b7de64b59b8c03b0bbfe14398b4ba
kimyoungsun <kyssmart@naver.com>
2018-08-09 09:39:44 +0900

    11111
    11111

--- Assets/Bound02_AreaSelect/BoundsSelect.cs
+++ Assets/Bound02_AreaSelect/BoundsSelect.cs
@@ -18,10 +18,13 @@ namespace Bound02{
 		void OnDrawGizmos(){
 			SpriteRenderer _r = GetComponent<SpriteRenderer> ();
 			if(_r != null){
-				if(bSelected)
-					Util.GizmosDrawBounds(_r.bounds, Color.green);
-				else
+				if (bSelected) {
+					_r.color = Color.red;
+					Util.GizmosDrawBounds (_r.bounds, Color.green);
+				} else {
+					_r.color = Color.white;
 					Gizmos2.DebugBounds (_r.bounds, Color.grey);
+				}
 			}
 		}
 	}
