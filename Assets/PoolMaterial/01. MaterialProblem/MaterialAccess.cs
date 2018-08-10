786689ab7b30617777e7c6dcd84a1e1336d23a97
kimyoungsun <kyssmart@naver.com>
2018-08-10 17:20:30 +0900

    iiii
    iii

---
+++ Assets/PoolMaterial/01. MaterialProblem/MaterialAccess.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PoolMaterialsProblem{
+	public class MaterialAccess : MonoBehaviour {
+		Renderer renderer;
+
+		void Start () {
+			renderer = GetComponent<Renderer> ();	
+		}
+
+		void Update () {
+			if (Input.GetMouseButtonDown (0)) {
+				renderer.material.color = Color.green;
+			}
+		}
+	}
+}
---
+++ Assets/PoolMaterial/02. MaterialSolves/MaterialAccess.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PoolMaterial1;
+
+namespace PoolMaterialsSolves{
+	public class MaterialAccess : MonoBehaviour {
+		Renderer renderer;
+		Material originalMaterial;
+		void Start () {
+			renderer 			= GetComponent<Renderer> ();
+			originalMaterial 	= renderer.sharedMaterial;
+			PoolMaterial.ins.RegisterMaterial ( originalMaterial );
+		}
+
+		void Update () {
+			if (Input.GetMouseButtonDown (0)) {
+				StopAllCoroutines ();
+				StartCoroutine (CoChange(2f));
+			}
+		}
+
+		IEnumerator CoChange(float _time){
+			float _percent = 0;
+			float _speed = 1f / _time;
+			MaterialData _newMaterialData 	= PoolMaterial.ins.GetMaterial (originalMaterial);
+			Material _newMaterial 			= _newMaterialData.mat;
+			renderer.sharedMaterial 		= _newMaterial;
+			Color _c1 = Color.clear;
+			Color _c2 = originalMaterial.color;
+
+			while(_percent < 1f){
+				_percent += Time.deltaTime * _speed;
+				_newMaterial.color = Color.Lerp (_c1, _c2, _percent);
+				yield return null;
+			}
+			_newMaterialData.Release ();
+			renderer.sharedMaterial = originalMaterial;
+		}
+	}
+}
