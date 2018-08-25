fb718ece5d68ee586e586dc11a9539b057cac75c
kimyoungsun <kyssmart@naver.com>
2018-08-25 12:54:46 +0900

    1111
    1111

--- Assets/TouchMove2/TouchManager.cs
+++ Assets/TouchMove2/TouchManager.cs
@@ -3,26 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-namespace TouchManager{
+namespace TouchManager2{
 	public class TouchManager : MonoBehaviour {
-		public static TouchManager ins;
+		public static TouchManager ins { get; private set; }
 		NavMeshAgent target;
 		Ray ray;
 		RaycastHit hit;
+		public float distance = 100f;
 
 		void Awake(){
 			ins = this;
 		}
 
-		void Update(){
+		void Update () {
 			if (Input.GetMouseButtonDown (0)) {
 				ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-				if (Physics.Raycast (ray, out hit, 100)) {
-					NavMeshAgent _target = hit.collider.GetComponent<NavMeshAgent> ();
-					if (_target != null) {
-						target = _target;
+				if (Physics.Raycast (ray, out hit, distance)) {
+					NavMeshAgent _agent = hit.collider.GetComponent<NavMeshAgent> ();
+					if (_agent != null) {
+						target = _agent;
+						//Debug.Log (11);
 					} else if(target != null) {
 						target.SetDestination (hit.point);
+						//Debug.Log (12);
 					}
 				}
 			}
