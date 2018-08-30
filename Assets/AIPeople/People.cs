b796594dd89ff0f0392ea7216a58c5dc552cc373
kimyoungsun <kyssmart@naver.com>
2018-08-30 16:45:08 +0900

    AIPeople
    AIPeople

---
+++ Assets/AIPeople/People.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace AIPeople{
+	public class People : MonoBehaviour {
+		NavMeshAgent agent;
+		float time;
+		public Vector2 TIME_WAIT = new Vector2(2f, 8f);
+		public List<Vector3> list = new List<Vector3> ();
+
+
+		// Use this for initialization
+		void Start () {
+			agent = GetComponent<NavMeshAgent> ();
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			if (Time.time > time) {
+
+				NextPosition ();
+			}
+		}
+
+		void NextPosition(){
+			time = Time.time + Random.Range(TIME_WAIT.x, TIME_WAIT.y);
+
+
+			Vector3 _pos = WayPoint.ins.GetPoint ().position;
+			agent.SetDestination (_pos);
+			list.Add (_pos);
+		}
+	}
+}
---
+++ Assets/AIPeople/WayPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AIPeople{
+	public class WayPoint : MonoBehaviour {
+		public static WayPoint ins { get; private set; }
+		List<Transform> list = new List<Transform>();
+
+		void Awake(){
+			ins = this;
+		}
+
+		void Start () {
+			foreach (Transform _t in transform) {
+				list.Add (_t);
+			}
+		}
+
+		public Transform GetPoint(){
+			return list [Random.Range(0,  list.Count -  1)];
+		}
+	}
+}
