dd34633134e264b2cbf6f7e830689d3fa996829b
kimyoungsun <kyssmart@naver.com>
2018-09-05 00:26:28 +0900

    111
    11

---
+++ Assets/PoolManager/PoolManager0_base/PoolManagerReturn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PoolManager0{
+	public class PoolManagerReturn: MonoBehaviour {
+		void OnEnable(){
+			Invoke ("Destroy", 5f);
+		}
+
+		void Destroy(){
+			gameObject.SetActive (false);
+		}
+
+		void OnDisable(){
+			CancelInvoke ();
+		}
+	}
+}
