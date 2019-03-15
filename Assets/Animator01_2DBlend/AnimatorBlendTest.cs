82a6bea3c0884f94beba10c1e426e095464016ea
kimyoungsun3 <40338139+kimyoungsun3@users.noreply.github.com>
2019-03-15 15:04:22 +0900

    444
    4444

---
+++ Assets/Animator01_2DBlend/AnimatorBlendTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AnimatorTest01
+{
+	public class AnimatorBlendTest : MonoBehaviour
+	{
+		Animator animator;
+		string strCharStats		= "CharStats";
+		float charStats			= 0;
+
+		void Start()
+		{
+			animator = GetComponent<Animator>();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Alpha1))
+			{
+				charStats = 0;
+			}
+			else if (Input.GetKeyDown(KeyCode.Alpha2))
+			{
+				charStats = 1;
+			}
+			else if (Input.GetKeyDown(KeyCode.Alpha3))
+			{
+				charStats = 2;
+			}
+
+			animator.SetFloat(strCharStats, charStats);
+		}
+	}
+}
