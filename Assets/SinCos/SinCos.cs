d8558f45b621002f3f15665c984cf6f8c9080741
kimyoungsun3 <kyssmart@naver.com>
2020-07-21 10:23:30 +0900

    3333

---
+++ Assets/SinCos/SinCos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace _SinCos
+{
+    public class SinCos : MonoBehaviour
+    {
+        public float speed = 90f;
+        Transform trans;
+        Vector3 pos;
+
+        void Start()
+        {
+            trans = transform;
+            pos = trans.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float _radian = Time.time * speed * Mathf.Deg2Rad;
+            float _x = Mathf.Cos(_radian);
+            float _y = Mathf.Sin(_radian);
+            pos.Set(_x, _y, pos.z);
+
+            trans.position = pos;
+        }
+    }
+}
